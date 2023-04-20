1da3a9b9a6346a86cddc7ee72ce718bf42b898e0
carlo.spinello <carlo.spinello@adesso.at>
2023-04-20 14:04:12 +0200

    feat: moved some central activities to here

---
+++ src/main/java/at/adesso/leagueapi/commons/errorhandling/ErrorResponseFactory.java
@@ -0,0 +1,62 @@
+package at.adesso.leagueapi.commons.errorhandling;
+
+import at.adesso.leagueapi.commons.errorhandling.error.Error;
+import at.adesso.leagueapi.commons.errorhandling.exception.ApiException;
+import at.adesso.leagueapi.commons.errorhandling.model.ErrorResponse;
+import at.adesso.leagueapi.commons.logging.CorrelationContext;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class ErrorResponseFactory {
+
+    public ErrorResponse createErrorResponse(final ApiException apiException) {
+        final ErrorResponse response = createNewResponse();
+
+        response.setMessage(apiException.getError().getMessage());
+        response.setCode(apiException.getError().getCode());
+        response.setDetails(apiException.getDetails());
+
+        return response;
+    }
+
+    public ErrorResponse createErrorResponse(final Error error) {
+        final ErrorResponse response = createNewResponse();
+
+        response.setMessage(error.getMessage());
+        response.setCode(error.getCode());
+        response.setDetails(Collections.emptyList());
+
+        return response;
+    }
+
+    public ErrorResponse createErrorResponse(final Error error, final String detail) {
+        final ErrorResponse response = createErrorResponse(error);
+
+        if (detail != null) {
+            response.setDetails(Collections.singletonList(detail));
+        }
+
+        return response;
+    }
+
+    public ErrorResponse createErrorResponse(final Error error, final List<String> details) {
+        final ErrorResponse response = createErrorResponse(error);
+
+        response.setDetails(details);
+
+        return response;
+    }
+
+    private ErrorResponse createNewResponse() {
+        final ErrorResponse response = new ErrorResponse();
+
+        response.setTimeStamp(LocalDateTime.now());
+        response.setCorrelationId(CorrelationContext.getId());
+
+        return response;
+    }
+}
---
+++ src/main/java/at/adesso/leagueapi/commons/errorhandling/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package at.adesso.leagueapi.commons.errorhandling.exception;
+
+
+import at.adesso.leagueapi.commons.errorhandling.error.CommonError;
+
+public class ResourceNotFoundException extends ApiException {
+
+    public ResourceNotFoundException() {
+        super(CommonError.RESOURCE_NOT_FOUND);
+    }
+
+}
